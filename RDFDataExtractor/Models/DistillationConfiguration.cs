a3dc543063427b7e5551417e7e241b570f07d53c
Marko Markoski <markoskiM@outlook.com>
2020-02-22 16:45:20 +0100

    ViewComponent  up and working.

---
+++ RDFDataExtractor/Models/DistillationConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RDFDataExtractor.Models
+{
+    public class DistillationConfiguration:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        => await Task.FromResult(View());
+    }
+}
