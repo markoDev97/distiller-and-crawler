a491821ab7eb55370a524141f1e3bea5f97acbf2
Marko Markoski <markoskiM@outlook.com>
2020-03-26 20:48:42 +0100

    Beginning to build necessary services.

---
+++ RDFDataExtractor/Models/Services/TextExtractionService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace RDFDataExtractor.Models.Services
+{
+    public class TextExtractionService
+    {
+        public string ExtractTextFromURI(string uri)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(uri);
+            var response = (HttpWebResponse)request.GetResponse();
+            var streamReader = 
+        }
+    }
+}
