d3833f7ed6674180381bc5b5e7111ad74d5217d0
Marko Markoski <markoskiM@outlook.com>
2020-03-26 21:39:45 +0100

    Structuring code in a good way.

--- RDFDataExtractor/Pages/Index.cshtml.cs
+++ RDFDataExtractor/Pages/Index.cshtml.cs
@@ -6,17 +6,23 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using RDFDataExtractor.Models.Services;
 
 namespace RDFDataExtractor.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly DistillationService _distillationService;
+        private readonly TextExtractionService _extractionService;
+
         public bool ShowOutput { get; private set; }
         public string WhichApproach { get;  private set; }
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, DistillationService distillationService, TextExtractionService extractionService)
         {
             _logger = logger;
+            _distillationService = distillationService;
+            _extractionService = extractionService;
         }
 
         public void OnGet()
@@ -30,15 +36,33 @@ namespace RDFDataExtractor.Pages
         public void OnPostFile(IFormFile fileForDistillation, string pageUri, string rawInput, string rdfa,
             string microdata, string turtle, string jsonLd, string format)
         {
-            ShowOutput = true;
+            if (fileForDistillation.ContentType == "text/html")
+            {
+                var html = _extractionService.ExtractTextFromFile(fileForDistillation);
+            }
+            else
+            {
+                ViewData["error"] = "Only .html files can be uploaded.";
+            }
         }
         public void OnPostURI(string pageUri, string rdfa, string microdata, string turtle, string jsonLd, string format)
         {
+            var html = _extractionService.ExtractTextFromURI(pageUri);
             ShowOutput = true;
         }
-        public void OnPostRawText(string pageUri, string rdfa, string microdata, string turtle, string jsonLd, string format)
+        public void OnPostRawText(string rawInput, string rdfa, string microdata, string turtle, string jsonLd, string format)
         {
             ShowOutput = true;
+        }
+        private ISet<string> WantedFormats(string rdfa, string microdata, string turtle, string jsonLd)
+        {
+            var res = new HashSet<string>();
+            return res;
+        }
+        private void AddIfOn(ref ISet<string> set, string format)
+        {
+            if (format == "on")
+                set.Add(format);
         }
     }
 }
